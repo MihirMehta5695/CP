b7a1384173450be48e0db56e42256df4c48ae276
Mihir Mehta <mihirm5695@gmail.com>
2023-01-02 17:52:38 +0100

    Completed 206 Reverse Linked List

---
+++ LeetCode/LeetCode75/Level1/206_Reverse_Linked_List/206_Reverse_Linked_List.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+        
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+
+        if (head is not None) and (head.next is None):
+            return head
+        list_vals = []
+        while head:
+            list_vals.append(head.val)
+            head = head.next
+        head = None
+        tail = None
+        for idx in range(len(list_vals)-1, -1, -1):
+            node = ListNode(list_vals[idx])
+            if head is None:
+                head = node
+                tail = head
+            tail.next = node
+            tail = tail.next
+        return head
